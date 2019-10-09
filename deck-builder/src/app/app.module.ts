a25ad48b4e6c2446305713249e4a1a7f27c945a2
Georgi-Delchev <georgi.delchev@adastragrp.com>
2019-10-09 14:44:53 +0300

    add material components; extract common module for components

--- deck-builder/src/app/app.module.ts
+++ deck-builder/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MaterialComponentsModule } from './material-components.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialComponentsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
