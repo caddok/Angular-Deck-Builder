b734ee4d8c774a018badc18ade56b6d39354ce44
Georgi-Delchev <georgi.delchev@adastragrp.com>
2019-10-10 13:56:55 +0300

    add Angular Flex Layout; add navigation toolbar; add images to be used from the app

--- deck-builder/src/app/material-components/material-components.module.ts
+++ deck-builder/src/app/material-components/material-components.module.ts
@@ -1,13 +1,19 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [],
   imports: [],
   exports: [
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatDividerModule,
+    MatListModule,
+    FlexLayoutModule
   ]
 })
 export class MaterialComponentsModule { }
